614d56f7e299353d76d190906b0769b1497d0982
Sanju Joseph <sanjuantony@gmail.com>
2021-08-26 16:57:31 -0400

    Testing out some composition API code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,32 @@
 <template>
   <h2>Home</h2>
-  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <p ref='para'>My name is {{ name }} and my age is {{ age }}</p>
+  <!-- <p ref='para_another'>2nd Paragraph</p> -->
   <button @click="handleClick">Click me</button>
 </template>
 
 <script>
+import {ref} from 'vue'
   export default {
     name: 'Home',
     setup() {
       console.log("setup called")
+      const para = ref(null)
+      // const para_another = ref(null)
+
 
       let name = "Sanju"
       let age = 40
 
       const handleClick = () => {
-        console.log("You clicked me")
+        console.log(para)
+        console.log("_rawValue: "+para._rawValue)
+        console.log("innerText: "+para.value.innerText)
+        para.value.classList.add("test")
+        // console.log(para_another, para_another.value)
       }
 
-      return {name, age, handleClick}
+      return {name, age, handleClick, para}
     }
   }
 </script>
